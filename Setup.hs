1421dc9e0cbf25b4c2208a727941bc7a4d693c65
Philipp Balzarek <p.balzarek@googlemail.com>
2014-03-02 22:55:47 +0100

    fix build on systems with nonstandard paths

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,47 @@
 import Distribution.Simple
-main = defaultMain
+import Distribution.PackageDescription
+import Distribution.Version
+
+import Distribution.Simple.LocalBuildInfo
+import Distribution.Simple.Program
+import Distribution.Verbosity
+
+import Control.Applicative
+import Control.Monad
+import Data.List
+import Data.Maybe
+
+main = defaultMainWithHooks simpleUserHooks {
+  hookedPrograms = [gpgmeConfigProgram],
+
+  confHook = \pkg flags -> do
+    lbi <- confHook simpleUserHooks pkg flags
+    bi <- psqlBuildInfo lbi
+    return lbi {
+      localPkgDescr = updatePackageDescription
+                        (Just bi, []) (localPkgDescr lbi)
+    }
+}
+
+gpgmeConfigProgram = simpleProgram "gpgme-config"
+
+
+psqlBuildInfo :: LocalBuildInfo -> IO BuildInfo
+psqlBuildInfo lbi = do
+  (pgconfigProg, _) <- requireProgram verbosity
+                         gpgmeConfigProgram (withPrograms lbi)
+  let gpgmeConfig = rawSystemProgramStdout verbosity pgconfigProg
+
+  incDirs <- hasPrefix "-I" <$> gpgmeConfig ["--cflags"]
+  libInfo <- gpgmeConfig ["--libs"]
+  let libDirs =  hasPrefix "-L"  libInfo
+      extraLibs = hasPrefix "-l" libInfo
+  return emptyBuildInfo
+      { includeDirs  = incDirs
+      , extraLibs = extraLibs
+      , extraLibDirs = libDirs
+
+  }
+  where
+    verbosity = normal -- honestly, this is a hack
+    hasPrefix pre = catMaybes . map (stripPrefix pre) . words
